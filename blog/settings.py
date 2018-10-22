6de20f1d54c4f91077ad5f594b4bc3552454bb60
Rafał Gawlik <rafal.gawlik@gmail.com>
2018-10-22 21:17:41 +0200

    update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -26,6 +26,8 @@ TEMPLATE_DEBUG = True
 
 ALLOWED_HOSTS = []
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Application definition
 
