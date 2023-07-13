296f3096267fa205b792ce5e409c94890669442a
Rafał Gawlik <rafal.gawlik@gmail.com>
2023-07-13 08:54:37 +0200

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ STATICFILES_DIRS = (
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#qlh4qmrle%$+i*c!(zt%n-2=9=3=vht&ka2-egmarrhe%p72c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
